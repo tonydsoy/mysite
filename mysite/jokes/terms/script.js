9367dba15cabe4168fa66b0f120df30274490c87
MushroomGuy12323 <97201792+MushroomGuy12323@users.noreply.github.com>
2024-12-19 07:00:45 -0700

    ghvbf fghytvc
    gvfthy bgtvybfhu

---
+++ mysite/jokes/terms/script.js
@@ -0,0 +1,7 @@
+var audio = document.getElementById("audio")
+var skbidi = document.getElementById("skibidi")
+
+function audios() {
+    skbidi.innerHTML = "<h2>1. </h2> <img type='image', src='kitty.jpeg'></img><br><h1>top 10 ways to avoid autoplay blocking</h1>"
+    audio.play()
+}
